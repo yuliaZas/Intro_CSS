39762b9ede05f3443b58efb63f4a23f74c95e5e9
yulia <yulia@LAPTOP-F4EG03HP>
2019-03-19 17:14:13 +0200

    Loaded the files to the project. Created the dictionary

---
+++ exer1/MissionContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+
+    public class FunctionsContainer
+    {
+        //ממפה בין STRING לפונקציות
+        public delegate double func(double value);
+        private Dictionary<string, func> funcDictionary;
+
+        public FunctionsContainer()
+        {
+
+        }
+
+        public List<string> getAllMissions()
+        {
+            return this.nameList;
+        }
+    }
+}

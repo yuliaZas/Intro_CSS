3f0790e6eed67bde3a64b229dc808215d97c04a8
yulia <yulia@LAPTOP-F4EG03HP>
2019-03-19 18:24:58 +0200

    creating the class MissionContainer.

--- exer1/MissionContainer.cs
+++ exer1/MissionContainer.cs
@@ -9,18 +9,30 @@ namespace Excercise_1
 
     public class FunctionsContainer
     {
-        //ממפה בין STRING לפונקציות
         public delegate double func(double value);
         private Dictionary<string, func> funcDictionary;
 
         public FunctionsContainer()
         {
-
+            funcDictionary = new Dictionary<string, func>();
         }
-
+        public func this[string name]
+        {
+            get
+            {
+                // return the value specified by index
+                return funcDictionary[name];
+            }
+            set
+            {
+                // set the value specified by index
+                funcDictionary[name] = value;
+            }
+        }
         public List<string> getAllMissions()
         {
-            return this.nameList;
+            List<string> nameList = new List<string>(this.funcDictionary.Keys);
+            return nameList;
         }
     }
 }
